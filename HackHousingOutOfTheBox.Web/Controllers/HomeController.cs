1afbe489c0e6ff442f5bebfdb19d82525c2bfc63
Adrian Limon <alimon808@gmail.com>
2015-02-08 04:14:15 -0800

    update mvc project - unable to use service to fetch data

--- HackHousingOutOfTheBox.Web/Controllers/HomeController.cs
+++ HackHousingOutOfTheBox.Web/Controllers/HomeController.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿//using SODA;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading.Tasks;
+using Esri.ArcGISRuntime.Tasks.Geocoding;
+using Esri.ArcGISRuntime.Geometry;
+using Esri.ArcGISRuntime.Tasks.Query;
+using HackHousingOutOfTheBox.Services;
+using Geocoding.Google;
+using Geocoding;
+using Newtonsoft.Json;
 
 namespace HackHousingOutOfTheBox.Web.Controllers
 {
@@ -10,6 +19,12 @@ namespace HackHousingOutOfTheBox.Web.Controllers
     {
         public ActionResult Index()
         {
+            GeocodingService geoCodingService = new GeocodingService();
+            LocationInfo locationInfo = geoCodingService.Geocode("150 GRANDVIEW WAY MISSOULA MT");
+
+            PublicHousingAuthorityInfoService phaiService = new PublicHousingAuthorityInfoService();
+            // Hangs
+            PublicHousingAuthorityInfo info = phaiService.GetPublicHousingAuthorityInfo(locationInfo.City, locationInfo.State);
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
